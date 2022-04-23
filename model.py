259eb2e50cc22850c55bf108197afa106ddcb0ae
Neetesh Dadwariya <neeteshdadwariya@gmail.com>
2022-04-23 15:21:20 -0500

    Model built

--- model.py
+++ model.py
@@ -5,10 +5,10 @@ import glob
 from keras.utils.np_utils import to_categorical
 from sklearn.model_selection import train_test_split
 
-from layers1.layers import ZeroPadding2D, Conv2D, BatchNormalization, Activation, MaxPooling2D, Flatten, Dense
-from layers1.loss_functions import SquareLoss, CrossEntropy
-from layers1.neural_network import NeuralNetwork
-from layers1.optimizers import Adam
+from layers.layers import ZeroPadding2D, Conv2D, BatchNormalization, Activation, MaxPooling2D, Flatten, Dense
+from layers.loss_functions import SquareLoss, CrossEntropy
+from layers.neural_network import NeuralNetwork
+from layers.optimizers import Adam
 
 
 class DeepLearningModel:
@@ -83,45 +83,50 @@ train_ds = train_ds.map(lambda x, y: (tf.keras.layers.Rescaling(1. / 255)(x), y)
 files = glob.glob("./seg_train/seg_train/**/*.*", recursive=True)
 print(len(files))
 
-batch = 0
-for image, label in train_ds:
-    batch += 1
-print('batches', batch)
+image_batch, labels_batch = next(iter(train_ds))
+first_image = image_batch[0]
+# Notice the pixel values are now in `[0,1]`.
+print(np.min(first_image), np.max(first_image))
 
-# image_batch, labels_batch = next(iter(train_ds))
-# first_image = image_batch[0]
-# # Notice the pixel values are now in `[0,1]`.
-# print(np.min(first_image), np.max(first_image))
-#
-# X = []
-# y = []
-# images, labels = next(iter(train_ds))
-# X = images.numpy()
-# y = labels.numpy()
-#
-# X = np.moveaxis(X, -1, 1)
-# # Convert to one-hot encoding
-# y = to_categorical(y.astype("int"))
-#
-# print(X.shape)
-# print(y.shape)
-#
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-#
-# model = DeepLearningModel(n_inputs=(3, 154, 154), n_outputs=6).get_model()
-# model.summary(name="DeepLearningModel")
-#
-# train_err, val_err = model.fit(X_train, y_train, n_epochs=1, batch_size=BATCH_SIZE)
-#
-# # Training and validation error plot
-# n = len(train_err)
-# # training, = plt.plot(range(n), train_err, label="Training Error")
-# # validation, = plt.plot(range(n), val_err, label="Validation Error")
-# # plt.legend(handles=[training, validation])
-# # plt.title("Error Plot")
-# # plt.ylabel('Error')
-# # plt.xlabel('Iterations')
-# # plt.show()
-#
-# _, accuracy = model.test_on_batch(X_test, y_test)
-# print ("Accuracy:", accuracy)
+X = []
+y = []
+
+for image_batch, label_batch in train_ds:
+    for i in range(BATCH_SIZE):
+        if i < image_batch.shape[0]:
+            X.append(image_batch[i].numpy())
+            y.append(label_batch[i].numpy())
+
+# MAX_SIZE = 10000
+# X = np.array(X)[:MAX_SIZE]
+# y = np.array(y)[:MAX_SIZE]
+
+X = np.array(X)
+y = np.array(y)
+
+X = np.moveaxis(X, -1, 1)
+# Convert to one-hot encoding
+y = to_categorical(y.astype("int"))
+
+print(X.shape)
+print(y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+model = DeepLearningModel(n_inputs=(3, 154, 154), n_outputs=6).get_model()
+model.summary(name="DeepLearningModel")
+
+train_err, val_err = model.fit(X_train, y_train, n_epochs=1, batch_size=BATCH_SIZE)
+
+# Training and validation error plot
+n = len(train_err)
+# training, = plt.plot(range(n), train_err, label="Training Error")
+# validation, = plt.plot(range(n), val_err, label="Validation Error")
+# plt.legend(handles=[training, validation])
+# plt.title("Error Plot")
+# plt.ylabel('Error')
+# plt.xlabel('Iterations')
+# plt.show()
+
+_, accuracy = model.test_on_batch(X_test, y_test)
+print ("Accuracy:", accuracy)

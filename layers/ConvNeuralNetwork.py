cf8e290b1d545f6bd6c9899d8ecc438d6a8d83c8
Neetesh Dadwariya <neeteshdadwariya@gmail.com>
2022-04-24 10:17:17 -0500

    Updated changes

--- layers/ConvNeuralNetwork.py
+++ layers/ConvNeuralNetwork.py
@@ -18,7 +18,6 @@ class NeuralNetwork:
 
     def add(self, layer):
         if self.layers:
-            print(self.layers[-1].name(), "output", self.layers[-1].get_output())
             layer.set_input(shape=self.layers[-1].get_output())
         if hasattr(layer, 'initialize'):
             layer.initialize(optimizer=self.optimizer)
@@ -41,24 +40,36 @@ class NeuralNetwork:
         return loss, accuracy
 
     def fit(self, X, y, n_epochs, batch_size):
+        train_acc = []
+        val_acc = []
+
         for i in range(n_epochs):
             batch_error = []
-            batch_accuracy = []
+            batch_train_accuracy = []
+            val_accuracy = 0
             batch = 0
             for X_batch, y_batch in batch_iterator(X, y, batch_size=batch_size):
-                loss, accuracy = self.train_on_batch(X_batch, y_batch)
+                loss, train_accuracy = self.train_on_batch(X_batch, y_batch)
                 batch_error.append(loss)
-                print("Training for epoch:{} batch:{} | loss={}, accuracy={}".format(i, batch, loss, accuracy))
+                batch_train_accuracy.append(train_accuracy)
+                # print("Training for epoch:{} batch:{} | loss={}, accuracy={}".format(i, batch, loss, train_accuracy))
                 batch += 1
 
-            self.errors["training"].append(np.mean(batch_error))
-
             if self.val_set is not None:
-                val_loss, accuracy = self.test_on_batch(self.val_set["X"], self.val_set["y"])
+                val_loss, val_accuracy = self.test_on_batch(self.val_set["X"], self.val_set["y"])
                 self.errors["validation"].append(val_loss)
-                batch_accuracy.append(accuracy)
 
-        return self.errors["training"], self.errors["validation"]
+            mean_training_loss = np.mean(batch_error)
+            mean_training_accuracy = np.mean(batch_train_accuracy)
+            train_acc.append(mean_training_accuracy)
+            val_acc.append(val_accuracy)
+
+            self.errors["training"].append(mean_training_loss)
+            print(
+                "Training loop epoch:{} | train_loss:{:.2f} train_accuracy:{:.2f} | val_loss:{:.2f} val_accuracy:{:.2f}"
+                .format(i, mean_training_loss, mean_training_accuracy, val_loss, val_accuracy))
+
+        return self.errors["training"], self.errors["validation"], train_acc, val_acc
 
     def _forward_pass(self, X, training=True):
         layer_output = X
